b35b16e2adaece180d46ecf4f8755f94aa4436dc
Harshit Mourya <mouryaharshit03@gmail.com>
2024-07-05 16:46:47 +0530

    default image

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,7 +4,7 @@ import newsImg from "../assets/news.jfif";
 export default function NewsItem({ title, description, src, url }) {
   return (
     <div className="card">
-      <img src={src ? src : newsImg} alt={title} />
+      <img src={src == "None" ? newsImg : src} alt={title} />
       <div className="cardBody">
         <p className="card-title">{title}</p>
         <p className="card-content">{description}</p>
